124164e64dbc52764c687018094cdf973e9f166a
MINDHELL <Tgpurpose640@gmail.com>
2025-02-22 14:53:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 BOT_TOKEN = "YOUR_BOT_TOKEN"
 MONGO_URL = "YOUR_MONGODB_URL"
-CHANNEL_ID = "-100"
+CHANNEL_ID = "-1002492623985"
 # Subscription settings
 FREE_LIMIT = 5  # Free users can fetch 5 files per day
 PREMIUM_LIMIT = 30  # Premium users can fetch 30 files per day
@@ -10,8 +10,8 @@ FREE_VIDEO_LENGTH = 300  # Max 5 min video for free users (300 sec)
 PREMIUM_VIDEO_LENGTH = None  # No limit for premium users
 
 # Payment and Contact Details
-OWNER_ID = 123456789  # Your Telegram ID
-PAYMENT_QR_IMAGE = "qr.jpg"  # Replace this with your actual QR code file
+OWNER_ID = 6860316927 # Your Telegram ID
+PAYMENT_QR_IMAGE = "https://envs.sh/apK.jpg"  # Replace this with your actual QR code file
 
 # Features Control
 FORCE_SUBSCRIBE = True  # Enable/Disable force subscription
